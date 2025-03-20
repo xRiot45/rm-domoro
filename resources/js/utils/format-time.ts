bdbb297f8a106329ad85a337696f597c6ddc32d2
rumahkodingku <rumahkodingku45@gmail.com>
2025-03-20 12:04:00 +0700

    feat: crud permissions completed

--- resources/js/utils/format-time.ts
+++ resources/js/utils/format-time.ts
@@ -1,26 +1,22 @@
 export const formatTime = (time: Date | string, format: string = 'hh:mm:ss A'): string => {
-  const pad = (num: number) => num.toString().padStart(2, '0');
+    const pad = (num: number) => num.toString().padStart(2, '0');
 
-  let hours: number, minutes: string, seconds: string, period: string;
+    let hours: number, minutes: string, seconds: string, period: string;
 
-  if (typeof time === 'string') {
-    const [h, m, s] = time.split(':').map(Number);
-    hours = h;
-    minutes = pad(m);
-    seconds = pad(s || 0);
-  } else {
-    hours = time.getHours();
-    minutes = pad(time.getMinutes());
-    seconds = pad(time.getSeconds());
-  }
+    if (typeof time === 'string') {
+        const [h, m, s] = time.split(':').map(Number);
+        hours = h;
+        minutes = pad(m);
+        seconds = pad(s || 0);
+    } else {
+        hours = time.getHours();
+        minutes = pad(time.getMinutes());
+        seconds = pad(time.getSeconds());
+    }
 
-  // eslint-disable-next-line prefer-const
-  period = hours >= 12 ? 'PM' : 'AM';
-  hours = hours % 12 || 12;
+    // eslint-disable-next-line prefer-const
+    period = hours >= 12 ? 'PM' : 'AM';
+    hours = hours % 12 || 12;
 
-  return format
-    .replace('hh', pad(hours))
-    .replace('mm', minutes)
-    .replace('ss', seconds)
-    .replace('A', period);
+    return format.replace('hh', pad(hours)).replace('mm', minutes).replace('ss', seconds).replace('A', period);
 };
