9fa09c7943e7e8aec670cbae48b388004c3aa7a8
rumahkodingku <rumahkodingku45@gmail.com>
2025-03-22 11:25:39 +0700

    style: update styling

--- resources/js/components/nav-user.tsx
+++ resources/js/components/nav-user.tsx
@@ -17,7 +17,10 @@ export function NavUser() {
             <SidebarMenuItem>
                 <DropdownMenu>
                     <DropdownMenuTrigger className="w-full">
-                        <SidebarMenuButton size="lg" className="text-sidebar-accent-foreground data-[state=open]:bg-sidebar-accent group">
+                        <SidebarMenuButton
+                            size="lg"
+                            className="text-sidebar-accent-foreground data-[state=open]:bg-sidebar-accent group cursor-pointer"
+                        >
                             <UserInfo user={auth.user} />
                             <ChevronsUpDown className="ml-auto size-4" />
                         </SidebarMenuButton>
--- resources/js/components/user-info.tsx
+++ resources/js/components/user-info.tsx
@@ -2,7 +2,7 @@ import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import { useInitials } from '@/hooks/use-initials';
 import { type User } from '@/types';
 
-export function UserInfo({ user, showEmail = false }: { user: User; showEmail?: boolean }) {
+export function UserInfo({ user, showEmail = true }: { user: User; showEmail?: boolean }) {
     const getInitials = useInitials();
 
     return (
