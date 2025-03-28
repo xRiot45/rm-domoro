747bc1a19d16a067179b0508d497f7982c243876
rumahkodingku <rumahkodingku45@gmail.com>
2025-03-28 19:36:47 +0700

    feat: create cart ui for customer

--- resources/js/layouts/app/layouts/lithium-layout/partials/footer.tsx
+++ resources/js/layouts/app/layouts/lithium-layout/partials/footer.tsx
@@ -1,6 +1,6 @@
 export function LithiumFooter() {
     return (
-        <footer className="border-grid border-t bg-black">
+        <footer className="border-grid mt-8 border-t bg-black">
             <div className="container-wrapper mx-auto max-w-7xl">
                 <div className="container py-4">
                     <h1 className="text-center text-sm leading-loose text-balance text-white md:text-left">Built by Thomas Alberto</h1>
