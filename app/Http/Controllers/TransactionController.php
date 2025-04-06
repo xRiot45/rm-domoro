3ea86e8f4bb2b21704015f184229ebacfa067882
rumahkodingku <rumahkodingku45@gmail.com>
2025-04-06 20:12:51 +0700

    refactor: refactoring transaction code

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -19,41 +19,37 @@ class TransactionController extends Controller
     public function payWithCash(TransactionRequest $request, Transaction $transaction): RedirectResponse
     {
         return DB::transaction(function () use ($request, $transaction) {
-            // Ambil biaya berdasarkan jenisnya
             $fees = Fee::whereIn(DB::raw('LOWER(type)'), ['delivery', 'service', 'discount', 'tax'])
                 ->get()
                 ->keyBy('type');
 
-            // Hitung subtotal
             $subtotal = $transaction->transactionItems->sum(fn($item) => $item->subtotal);
 
-            // Tentukan jenis pesanan dan metode pembayaran
             $orderType = OrderTypeEnum::tryFrom($request->order_type) ?? OrderTypeEnum::DineIn;
-            $paymentType = PaymentMethodEnum::tryFrom($request->payment_method) ?? PaymentMethodEnum::Cash;
+            $paymentMethod = PaymentMethodEnum::tryFrom($request->payment_method) ?? PaymentMethodEnum::Cash;
 
-            // Hitung biaya tambahan
             $deliveryFee = $orderType === OrderTypeEnum::Delivery ? $fees['delivery']->amount ?? 0 : 0;
-            $serviceFee = $fees['service']->amount ?? 0;
+
+            $serviceCharge = $fees['service']->amount ?? 0;
             $discount = $fees['discount']->amount ?? 0;
             $tax = $fees['tax']->amount ?? 0;
 
-            // Hitung total akhir
-            $finalTotal = $subtotal + $deliveryFee + $serviceFee - $discount + $tax;
+            $finalTotal = $subtotal + $deliveryFee + $serviceCharge - $discount + $tax;
 
-            // Validasi jika pembayaran tunai tidak mencukupi
-            if ($paymentType === PaymentMethodEnum::Cash && $request->cash_received < $finalTotal) {
+            if ($paymentMethod === PaymentMethodEnum::Cash && $request->cash_received < $finalTotal) {
                 return redirect()
                     ->back()
                     ->withErrors(['cash_received' => 'Uang Anda kurang.']);
             }
 
-            // Perbarui transaksi
+            $change = $request->cash_received - $finalTotal;
+
             $transaction->update([
-                'order_type' => $request->order_type,
-                'payment_method' => $request->payment_method,
-                'payment_status' => $paymentType === PaymentMethodEnum::Cash ? PaymentStatusEnum::Paid : PaymentStatusEnum::Pending,
+                'order_type' => $orderType,
+                'payment_method' => $paymentMethod,
+                'payment_status' => $paymentMethod === PaymentMethodEnum::Cash ? PaymentStatusEnum::Paid : PaymentStatusEnum::Pending,
                 'cash_received' => $request->cash_received,
-                'change' => $request->cash_received - $finalTotal,
+                'change' => $change,
                 'table_number' => $request->table_number,
                 'shipping_address' => $request->shipping_address,
                 'recipient' => $request->recipient,
@@ -63,7 +59,7 @@ class TransactionController extends Controller
                 'courier_id' => null,
                 'final_total' => $finalTotal,
                 'delivery_fee' => $deliveryFee,
-                'service_charge' => $serviceFee,
+                'service_charge' => $serviceCharge,
                 'discount' => $discount,
                 'tax' => $tax,
             ]);
@@ -75,44 +71,38 @@ class TransactionController extends Controller
     public function payWithMidtrans(TransactionRequest $request, Transaction $transaction): RedirectResponse
     {
         $transaction->load('transactionItems.menuItem');
-
         $transaction->note = $request->input('note');
 
+        // Tentukan jenis pesanan
         $orderType = OrderTypeEnum::tryFrom($request->order_type) ?? OrderTypeEnum::DineIn;
         $transaction->order_type = $orderType;
 
+        // Isi informasi tambahan berdasarkan jenis pesanan
         if ($orderType === OrderTypeEnum::DineIn && $request->has('table_number')) {
-            $transaction->order_type = OrderTypeEnum::DineIn;
             $transaction->table_number = $request->input('table_number');
         } elseif ($orderType === OrderTypeEnum::Takeway && $request->has('recipient')) {
-            $transaction->order_type = OrderTypeEnum::Takeway;
             $transaction->recipient = $request->input('recipient');
         } elseif ($orderType === OrderTypeEnum::Delivery && $request->has('shipping_address') && $request->has('recipient') && $request->has('recipient_phone_number')) {
-            $transaction->order_type = OrderTypeEnum::Delivery;
             $transaction->shipping_address = $request->input('shipping_address');
             $transaction->recipient = $request->input('recipient');
             $transaction->recipient_phone_number = $request->input('recipient_phone_number');
         } elseif ($orderType === OrderTypeEnum::Pickup && $request->has('recipient') && $request->has('recipient_phone_number')) {
-            $transaction->order_type = OrderTypeEnum::Pickup;
             $transaction->recipient = $request->input('recipient');
             $transaction->recipient_phone_number = $request->input('recipient_phone_number');
         }
 
-        // Ambil biaya berdasarkan jenisnya
+        // Ambil biaya lainnya
         $fees = Fee::whereIn(DB::raw('LOWER(type)'), ['delivery', 'service', 'discount', 'tax'])
             ->get()
             ->keyBy('type');
 
-        // Hitung biaya tambahan
         $deliveryFee = $orderType === OrderTypeEnum::Delivery ? $fees['delivery']->amount ?? 0 : 0;
         $serviceFee = $fees['service']->amount ?? 0;
         $discount = $fees['discount']->amount ?? 0;
         $tax = $fees['tax']->amount ?? 0;
 
-        // Hitung subtotal
+        // Hitung subtotal & total
         $subtotal = $transaction->transactionItems->sum(fn($item) => $item->subtotal);
-
-        // Hitung total akhir
         $finalTotal = $subtotal + $deliveryFee + $serviceFee - $discount + $tax;
 
         // Simpan ke database
@@ -123,6 +113,7 @@ class TransactionController extends Controller
         $transaction->final_total = $finalTotal;
         $transaction->save();
 
+        // Buat item detail untuk midtrans
         $itemDetails = $transaction->transactionItems
             ->map(function ($item) {
                 return [
@@ -171,7 +162,13 @@ class TransactionController extends Controller
             ];
         }
 
-        // Buat transaksi midtrans
+        // Konfigurasi Midtrans
+        \Midtrans\Config::$serverKey = config('services.midtrans.server_key');
+        \Midtrans\Config::$isProduction = config('services.midtrans.is_production');
+        \Midtrans\Config::$isSanitized = config('services.midtrans.is_sanitized');
+        \Midtrans\Config::$is3ds = config('services.midtrans.is_3ds');
+
+        // Parameter ke Midtrans
         $midtransParams = [
             'transaction_details' => [
                 'order_id' => $transaction->order_number,
@@ -193,68 +190,56 @@ class TransactionController extends Controller
             ],
         ];
 
-        // Panggil Midtrans API
-        \Midtrans\Config::$serverKey = config('services.midtrans.server_key');
-        \Midtrans\Config::$isProduction = config('services.midtrans.is_production');
-        \Midtrans\Config::$isSanitized = config('services.midtrans.is_sanitized');
-        \Midtrans\Config::$is3ds = config('services.midtrans.is_3ds');
-
+        // Ambil Snap Token
         $snapToken = \Midtrans\Snap::getSnapToken($midtransParams);
 
         return redirect()
             ->back()
-            ->with([
-                'snap_token' => $snapToken,
-            ]);
+            ->with(['snap_token' => $snapToken]);
     }
 
     public function midtransCallback(Request $request): RedirectResponse
     {
-        $orderId = $request->input('order_id');
+        $orderId = $request->input('order_id'); // Ambil order_id dari Midtrans
         $transaction = Transaction::where('order_number', $orderId)->first();
 
         if (!$transaction) {
             return redirect()->back()->withErrors('Transaksi tidak ditemukan.');
         }
 
-        $transactionStatus = $request->input('transaction_status');
+        $paymnetStatusMap = [
+            'settlement' => PaymentStatusEnum::Paid,
+            'capture'    => PaymentStatusEnum::Paid,
+            'pending'    => PaymentStatusEnum::Pending,
+            'expire'     => PaymentStatusEnum::Expired,
+            'cancel'     => PaymentStatusEnum::Cancelled,
+            'deny'       => PaymentStatusEnum::Failed,
+            'failure'    => PaymentStatusEnum::Failed,
+            'refund'     => PaymentStatusEnum::Refunded,
+        ];
 
-        switch ($transactionStatus) {
-            case 'settlement':
-            case 'capture':
-                $transaction->payment_status = PaymentStatusEnum::Paid;
-                break;
-            case 'pending':
-                $transaction->payment_status = PaymentStatusEnum::Pending;
-                break;
-            case 'expire':
-                $transaction->payment_status = PaymentStatusEnum::Expired;
-                break;
-            case 'cancel':
-                $transaction->payment_status = PaymentStatusEnum::Cancelled;
-                break;
-            case 'deny':
-            case 'failure':
-                $transaction->payment_status = PaymentStatusEnum::Failed;
-                break;
-            case 'refund':
-                $transaction->payment_status = PaymentStatusEnum::Refunded;
-                break;
-        }
-
+        $transactionStatus = $request->input('transaction_status'); // Ambil transaction_status dari Midtrans
+        $transaction->payment_status = $paymnetStatusMap[$transactionStatus] ?? $transaction->payment_status;
         $transaction->payment_method = PaymentMethodEnum::OnlinePayment;
         $transaction->save();
 
+        $isSuccess = $transactionStatus === 'settlement';
+
         if ($transaction->cashier_id) {
-            return $transactionStatus === 'settlement' ? redirect()->route('cashier.transaction.success') : redirect()->route('cashier.transaction.failed');
+            return $isSuccess
+                ? redirect()->route('cashier.transaction.success')
+                : redirect()->route('cashier.transaction.failed');
         }
 
         if ($transaction->customer_id) {
-            return $transactionStatus === 'settlement' ? redirect()->route('transaction.success') : redirect()->route('transaction.failed');
+            return $isSuccess
+                ? redirect()->route('transaction.success')
+                : redirect()->route('transaction.failed');
         }
 
         return redirect()->back()->withErrors('Tidak dapat menentukan role pengguna.');
     }
+
 
     // Untuk Customer
     public function transactionCustomerSuccess(): Response
