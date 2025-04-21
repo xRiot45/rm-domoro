3559cfb16d08529df7fd8dbdf4a27ee36c1d073b
rumahkodingku <rumahkodingku45@gmail.com>
2025-04-21 10:54:31 +0700

    feat: added modal ui for detail menu items

--- resources/js/pages/customer/components/menu-item-card.tsx
+++ resources/js/pages/customer/components/menu-item-card.tsx
@@ -1,4 +1,6 @@
+import { Badge } from '@/components/ui/badge';
 import { Button } from '@/components/ui/button';
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle } from '@/components/ui/dialog';
 import { cn } from '@/lib/utils';
 import { MenuItems } from '@/models/menu-items';
 import { User } from '@/models/user';
@@ -17,6 +19,9 @@ export default function MenuItemCard({ menuItems }: MenuItemCardProps) {
     const { props } = usePage<{ auth: { user: User; wishlist: number[] } }>();
     const user = props.auth?.user;
     const { addMenuToCart } = useCart();
+    const [selectedItem, setSelectedItem] = useState<MenuItems | null>(null);
+
+    console.log(menuItems);
 
     const [wishlistItems, setWishlistItems] = useState<number[]>(props.auth?.wishlist || []);
 
@@ -54,11 +59,11 @@ export default function MenuItemCard({ menuItems }: MenuItemCardProps) {
     };
 
     return (
-        <div className="mb-20 grid w-full flex-1 auto-rows-min grid-cols-2 gap-4 space-y-5 md:grid-cols-2 lg:grid-cols-3">
+        <div className="mb-20 grid w-full flex-1 auto-rows-min grid-cols-2 gap-5 space-y-5 md:grid-cols-2 lg:grid-cols-3">
             {menuItems?.map((item) => (
                 <div key={item.id} className={cn('pb-0.5')}>
                     <div className="relative">
-                        <div className="relative mx-auto aspect-square w-full overflow-hidden rounded-md">
+                        <div className="relative mx-auto aspect-square w-full overflow-hidden rounded-xl">
                             <img
                                 alt={item?.name}
                                 src={`${item?.image_url}`}
@@ -80,14 +85,13 @@ export default function MenuItemCard({ menuItems }: MenuItemCardProps) {
                         </div>
                     </div>
                     <div className="pt-3">
-                        <div className="flex justify-between">
-                            <div>
-                                <span className="text-sm font-medium dark:text-gray-200">{item?.menu_category?.name}</span>
-                                <h1 className="my-1 truncate font-bold">{item?.name}</h1>
-                                <span className="flex items-center text-sm font-medium text-gray-800 dark:text-gray-200">
-                                    {formatCurrency(Number(item?.price))}
-                                </span>
-                            </div>
+                        <div onClick={() => setSelectedItem(item)} className="cursor-pointer">
+                            <span className="text-sm font-medium dark:text-gray-200">{item?.menu_category?.name}</span>
+                            <h1 className="my-1 truncate font-bold">{item?.name}</h1>
+
+                            <span className="flex items-center text-sm font-medium text-gray-800 dark:text-gray-200">
+                                {formatCurrency(Number(item?.price))}
+                            </span>
                         </div>
                         <div className="mt-4 space-y-3">
                             <Button
@@ -114,6 +118,54 @@ export default function MenuItemCard({ menuItems }: MenuItemCardProps) {
                             )}
                         </div>
                     </div>
+
+                    <Dialog open={!!selectedItem} onOpenChange={() => setSelectedItem(null)}>
+                        <DialogContent className="overflow-hidden rounded-xl border-none p-0 shadow-none dark:bg-zinc-900">
+                            {selectedItem && (
+                                <div className="flex flex-col bg-white md:flex-row dark:bg-zinc-900">
+                                    {/* Gambar */}
+                                    <div className="w-full md:w-1/2">
+                                        <img src={`${selectedItem.image_url}`} alt={selectedItem.name} className="h-full w-full object-cover" />
+                                    </div>
+
+                                    {/* Detail */}
+                                    <div className="flex w-full flex-col justify-between space-y-4 p-6 md:w-1/2">
+                                        <div className="space-y-4">
+                                            <DialogHeader className="text-start">
+                                                <DialogTitle className="text-xl font-bold text-black dark:text-white">
+                                                    {selectedItem.name}
+                                                </DialogTitle>
+                                                <DialogDescription className="text-muted-foreground text-sm dark:text-zinc-400">
+                                                    {selectedItem.menu_category.name}
+                                                </DialogDescription>
+                                            </DialogHeader>
+
+                                            <div className="flex items-center justify-between">
+                                                <p className="text-lg font-semibold text-black dark:text-white">
+                                                    {formatCurrency(Number(selectedItem.price))}
+                                                </p>
+                                                <Badge
+                                                    variant={selectedItem.status === 'tersedia' ? 'default' : 'destructive'}
+                                                    className="capitalize"
+                                                >
+                                                    {selectedItem.status}
+                                                </Badge>
+                                            </div>
+
+                                            <div>
+                                                <h4 className="mb-2 text-sm font-semibold text-gray-700 dark:text-zinc-200">Bahan-Bahan:</h4>
+                                                <ul className="list-inside list-disc space-y-1 text-sm text-gray-600 dark:text-zinc-400">
+                                                    {selectedItem.ingredients.map((ingredient, index) => (
+                                                        <li key={index}>{ingredient}</li>
+                                                    ))}
+                                                </ul>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            )}
+                        </DialogContent>
+                    </Dialog>
                 </div>
             ))}
         </div>
