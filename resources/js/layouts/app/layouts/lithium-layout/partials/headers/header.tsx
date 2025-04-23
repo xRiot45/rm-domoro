124a9ffa1dd718ff0cd339d2934168d824a45f2a
rumahkodingku <rumahkodingku45@gmail.com>
2025-04-23 11:02:21 +0700

    feat: added alert in cart page if customer not yet fill in address

--- resources/js/layouts/app/layouts/lithium-layout/partials/headers/header.tsx
+++ resources/js/layouts/app/layouts/lithium-layout/partials/headers/header.tsx
@@ -3,14 +3,11 @@ import { Breadcrumbs } from '@/components/breadcrumbs';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import { Button } from '@/components/ui/button';
 import { DropdownMenu, DropdownMenuContent, DropdownMenuTrigger } from '@/components/ui/dropdown-menu';
-import { NavigationMenu, NavigationMenuItem, NavigationMenuList, navigationMenuTriggerStyle } from '@/components/ui/navigation-menu';
 import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@/components/ui/tooltip';
 import { useInitials } from '@/hooks/use-initials';
-import { cn } from '@/lib/utils';
 import { type BreadcrumbItem, type SharedData } from '@/types';
 import { Icon } from '@iconify/react';
 import { Link, usePage } from '@inertiajs/react';
-import mainNavItems from './main-nav-items';
 import { CustomerMenuContent } from './menu-content';
 import rightNavItems from './right-nav-items';
 
