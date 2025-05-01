90fc0a6e8aafbfb1a2e0fd5ced447db7d59bfbc8
rumahkodingku <rumahkodingku45@gmail.com>
2025-05-01 09:16:48 +0700

    refactor: delete button for remove data

--- resources/js/pages/admin/pages/financial-reports/revenue/partials/table/data-table-row-actions.tsx
+++ resources/js/pages/admin/pages/financial-reports/revenue/partials/table/data-table-row-actions.tsx
@@ -1,23 +1,5 @@
-import {
-    AlertDialog,
-    AlertDialogAction,
-    AlertDialogCancel,
-    AlertDialogContent,
-    AlertDialogDescription,
-    AlertDialogFooter,
-    AlertDialogHeader,
-    AlertDialogTitle,
-    AlertDialogTrigger,
-} from '@/components/ui/alert-dialog';
 import { Button } from '@/components/ui/button';
-import {
-    DropdownMenu,
-    DropdownMenuContent,
-    DropdownMenuItem,
-    DropdownMenuSeparator,
-    DropdownMenuShortcut,
-    DropdownMenuTrigger,
-} from '@/components/ui/dropdown-menu';
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuShortcut, DropdownMenuTrigger } from '@/components/ui/dropdown-menu';
 import { RevenueReport } from '@/models/financial-reports';
 import { Icon } from '@iconify/react';
 import { Link } from '@inertiajs/react';
@@ -25,10 +7,6 @@ import { DotsHorizontalIcon } from '@radix-ui/react-icons';
 import { Row } from '@tanstack/react-table';
 
 export function DataTableRowActions({ row }: { row: Row<RevenueReport> }) {
-    const handleDelete = (revenueReportId: number) => {
-        console.log(revenueReportId);
-    };
-
     return (
         <>
             <DropdownMenu>
@@ -50,29 +28,6 @@ export function DataTableRowActions({ row }: { row: Row<RevenueReport> }) {
                             </DropdownMenuShortcut>
                         </DropdownMenuItem>
                     </Link>
-                    <DropdownMenuSeparator />
-                    <AlertDialog>
-                        <AlertDialogTrigger asChild>
-                            <DropdownMenuItem className="cursor-pointer !text-red-500" onSelect={(e) => e.preventDefault()}>
-                                Hapus Data
-                                <DropdownMenuShortcut>
-                                    <Icon icon={'material-symbols:delete'} className="!text-red-500" />
-                                </DropdownMenuShortcut>
-                            </DropdownMenuItem>
-                        </AlertDialogTrigger>
-                        <AlertDialogContent>
-                            <AlertDialogHeader>
-                                <AlertDialogTitle>Hapus Data</AlertDialogTitle>
-                                <AlertDialogDescription>Apakah Kamu Yakin Ingin Menghapus Data ini?</AlertDialogDescription>
-                            </AlertDialogHeader>
-                            <AlertDialogFooter>
-                                <AlertDialogCancel className="cursor-pointer">Batal</AlertDialogCancel>
-                                <AlertDialogAction onClick={() => handleDelete(row.original.id)} className="cursor-pointer bg-red-600 transition-all">
-                                    Hapus
-                                </AlertDialogAction>
-                            </AlertDialogFooter>
-                        </AlertDialogContent>
-                    </AlertDialog>
                 </DropdownMenuContent>
             </DropdownMenu>
         </>
