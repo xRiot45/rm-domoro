b477cd5411032cd2cd76db9e33986bc02deab5fc
rumahkodingku <rumahkodingku45@gmail.com>
2025-05-01 10:15:10 +0700

    feat: report date range filter for revenue report

--- resources/js/pages/admin/pages/financial-reports/revenue/partials/table/data-table-toolbar.tsx
+++ resources/js/pages/admin/pages/financial-reports/revenue/partials/table/data-table-toolbar.tsx
@@ -1,38 +1,24 @@
-import {
-    AlertDialog,
-    AlertDialogAction,
-    AlertDialogCancel,
-    AlertDialogContent,
-    AlertDialogDescription,
-    AlertDialogFooter,
-    AlertDialogHeader,
-    AlertDialogTitle,
-} from '@/components/ui/alert-dialog';
 import { Button } from '@/components/ui/button';
-import { Input } from '@/components/ui/input';
 import { DataTableToolbarProps } from '@/types/tanstack';
 import { Icon } from '@iconify/react';
-import { useState } from 'react';
+import { DateRangeFacetedFilter } from './data-dateRange-faceted-filter';
+import { RevenueRangeFilter } from './data-revenue-filter';
+import { TransactionRangeFilter } from './data-transaction-filter';
 
 export function DataTableToolbar<TData>({ table }: DataTableToolbarProps<TData>) {
     const isFiltered = table.getState().columnFilters.length > 0;
-    const isAllSelected = table.getIsAllPageRowsSelected();
-
-    const [open, setOpen] = useState<boolean>(false);
-
-    const handleDeleteAll = () => {
-        console.log('handleDeleteAll');
-    };
 
     return (
         <div className="flex items-center justify-between">
             <div className="flex flex-1 flex-col-reverse items-start gap-y-2 sm:flex-row sm:items-center sm:space-x-2">
-                <Input
-                    placeholder="Cari laporan pemasukan..."
-                    value={(table.getColumn('name')?.getFilterValue() as string) ?? ''}
-                    onChange={(event) => table.getColumn('name')?.setFilterValue(event.target.value)}
-                    className="h-8 w-[150px] lg:w-[250px]"
-                />
+                {table.getColumn('report_date') && <DateRangeFacetedFilter column={table.getColumn('report_date')} title="Rentang Tanggal Laporan" />}
+
+                {table.getColumn('total_transactions') && (
+                    <TransactionRangeFilter column={table.getColumn('total_transactions')} title="Total Transaksi" />
+                )}
+
+                {table.getColumn('total_revenue') && <RevenueRangeFilter column={table.getColumn('total_revenue')} title="Total Pendapatan" />}
+
                 {isFiltered && (
                     <Button variant="ghost" onClick={() => table.resetColumnFilters()} className="h-8 cursor-pointer px-2 lg:px-3">
                         Reset
@@ -40,33 +26,6 @@ export function DataTableToolbar<TData>({ table }: DataTableToolbarProps<TData>)
                     </Button>
                 )}
             </div>
-
-            {isAllSelected && (
-                <div>
-                    <Button
-                        variant="outline"
-                        onClick={() => setOpen(true)}
-                        className="h-8 cursor-pointer border border-red-500 px-2 text-red-500 hover:bg-transparent lg:px-3"
-                    >
-                        Hapus semua data
-                        <Icon icon={'material-symbols:delete'} className="h-4 w-4" />
-                    </Button>
-                    <AlertDialog open={open} onOpenChange={setOpen}>
-                        <AlertDialogContent>
-                            <AlertDialogHeader>
-                                <AlertDialogTitle>Hapus Data</AlertDialogTitle>
-                                <AlertDialogDescription>Apakah Kamu Yakin Ingin Menghapus Semua Data?</AlertDialogDescription>
-                            </AlertDialogHeader>
-                            <AlertDialogFooter>
-                                <AlertDialogCancel className="cursor-pointer">Batal</AlertDialogCancel>
-                                <AlertDialogAction className="cursor-pointer bg-red-600 transition-all" onClick={handleDeleteAll}>
-                                    Hapus
-                                </AlertDialogAction>
-                            </AlertDialogFooter>
-                        </AlertDialogContent>
-                    </AlertDialog>
-                </div>
-            )}
         </div>
     );
 }
